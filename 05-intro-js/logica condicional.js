fbd17d9059e2151bf9c0b3e10c82fd328db57e1d
Alejandra Franco <210068@mail.unacar.mx>
2023-03-10 13:04:41 -0600

    segundo commit

--- 05-intro-js/logica condicional.js
+++ 05-intro-js/logica condicional.js
@@ -4,7 +4,9 @@ prompt("¿Ya hiciste tu tarea?")
 
 if(tareaHecha ===  'SI'){
      alert('¡Bien hecho!')
+} else if (tareaHhecha === 'NO'){
+     alert('No te rindas, ¡empieza ya mismo!')
 
 } else {
-    alert('No te rindas, ¡empieza ya mismo!')
+    
 }
