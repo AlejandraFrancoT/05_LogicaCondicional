37d50da087d40fa18b495784d6df5e16382a4f4b
Alejandra Franco <210068@mail.unacar.mx>
2023-03-11 01:32:15 -0600

    tercer comit

---
+++ 05-intro-js/Grados.js
@@ -0,0 +1,6 @@
+var temperatura= prompt("¿Cuántos grados Fahrenheit?")
+console.log(temperatura);
+
+function convertirCelsius(){
+     var celsius= (grados Farenheit- 32) * (5/9);
+}
--- 05-intro-js/Tarea.js
+++ 05-intro-js/Tarea.js
@@ -1,10 +1,10 @@
-var tareaHecha = 'NO'
-
-prompt("¿Ya hiciste tu tarea?")
+var tareaHecha = prompt("¿Ya hiciste tu tarea?")
+console.log(tareaHecha);
 
 if(tareaHecha ===  'SI'){
      alert('¡Bien hecho!')
+
 } else if (tareaHecha === 'NO'){
      alert('No te rindas, ¡empieza ya mismo!')
+}
 
-} 
